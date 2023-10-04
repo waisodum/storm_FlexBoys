bbe9a5304560cf662debc096b83dab4811b9f524
sarvesh0202 <sarveshparab002@gmail.com>
2023-10-04 11:54:27 +0530

    salman

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Home from './pages/home'; 
 
 function App() {
   return (
-    <div className="bg-emerald-50">
-heloooooooooooooooooooooo
-    </div>
+    <>
+<Home/>
+</>
   );
 }
 
---
+++ my-app/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Carousel/>
+    </> 
+  )
+}
+
+export default Home
