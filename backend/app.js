30e298ecc0778be8b64acfd3993707c68c37a2b9
salman <salman.iqureshi04@gmail.com>
2023-10-04 18:11:32 +0530

    salman

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-
+const corsconfig={
+  origin: '*', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+  allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+  preflightContinue: false, 
+  optionsSuccessStatus: 204, 
+};
+app.use(cors(corsconfig))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ backend/models/eventlist.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+try {
+    
+    mongoose.connect("mongodb://127.0.0.1:27017/Uploads");
+    const db = mongoose.connection;
+    db.once("open", () => {
+      console.log("connected to uploads");
+    });
+} catch (error) {
+    console.log("error");
+}
+
+
+
+const UploadSchema = new mongoose.Schema({
+  category:String,
+  name:String,
+  Desc:String
+});
+
+const event = new mongoose.model("event", UploadSchema);
+module.exports = event;
