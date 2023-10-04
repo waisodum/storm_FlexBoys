a423cda2e239561ff1fd5085bd5d6e61984d2228
salman <salman.iqureshi04@gmail.com>
2023-10-04 11:49:03 +0530

    salman

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,22 @@
+import ExampleV2 from './components/login';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './components/navbar';
+import Home from "./components/Home";
 
 function App() {
+const [nav , setNav]=useState(true);
+
   return (
-    <div className="bg-emerald-50">
-heloooooooooooooooooooooo
-    </div>
+    <BrowserRouter>
+  {nav?<Navbar/>:<>salman</>}
+  <Routes>
+<Route path='/' element={<Home/>}></Route>
+<Route path='/Product'  ></Route>
+
+  </Routes>
+   </BrowserRouter>
   );
 }
 
